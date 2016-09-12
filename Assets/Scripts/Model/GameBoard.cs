1fcc9c454f8d6e8ac36120dc34e1257389502a40
Richard Simpson <richard.simpson@king.com>
2016-09-12 21:58:38 +0100

    Added a description of the game board

--- Assets/Scripts/Model/GameBoard.cs
+++ Assets/Scripts/Model/GameBoard.cs
@@ -1,5 +1,21 @@
 ﻿using System;
 
+// Game Board Description
+//
+// Each side of the game board follows this pattern:
+//
+// P1--2--3--[] 
+//
+// Where:
+//     P is the column that the player moved up and down in
+//     1,2,3 are the locations of board objects (splitters, terminators, etc
+//     [] is the central column
+//
+// When firing a 'zap', the zap is moved into column '1'.
+//
+// Note that if there is a terminator in '1', then the player cannot fire into that row.
+// Note also that a player cannot fire into a row that alrady has an active zap.
+
 public class GameBoard
 {
     private Target[] targets = new Target[12];

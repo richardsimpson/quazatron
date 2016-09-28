b9aeec944c973ecc60315955f7325e66b6c57fb9
Richard Simpson <richard.simpson@king.com>
2016-09-28 21:53:25 +0100

    Added simple start screen

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene("minigame");
+    }
+
+}
